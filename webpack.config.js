d30c61f2107a88a8fd4fd61b5a25001c08695eaf
zdr <samsal26@gmail.com>
2017-12-26 13:49:56 +0500

    Completed desktop and mobile header styles

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+    entry: "./app/assets/scripts/app.js",
+    output: {
+        path: __dirname + '/app/temp/scripts',
+        filename: "app.js"
+    },
+    module: {
+        loaders: [{
+                loader: 'babel-loader',
+                query: {
+                    presets: ['es2015']
+                },
+                test: /\.js$/,
+                exclude: /node_modules/
+            }
+        ]
+    }
+}
